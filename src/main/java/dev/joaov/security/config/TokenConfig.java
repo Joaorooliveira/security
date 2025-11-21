a93cfbddb21b1990677360fc6e61f07233f7a9f0
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-20 22:51:25 -0300

    feat(aut-controller): Adiciona no endpoint login uma chamada para geracao de token e retorna ao usuario

---
+++ src/main/java/dev/joaov/security/config/TokenConfig.java
@@ -0,0 +1,4 @@
+package dev.joaov.security.config;
+
+public class TokenConfig {
+}
--- src/main/java/dev/joaov/security/controller/AuthController.java
+++ src/main/java/dev/joaov/security/controller/AuthController.java
@@ -1,5 +1,6 @@
 package dev.joaov.security.controller;
 
+import dev.joaov.security.config.TokenConfig;
 import dev.joaov.security.entity.User;
 import dev.joaov.security.dto.request.LoginRequest;
 import dev.joaov.security.dto.request.RegisterUserRequest;
@@ -9,6 +10,11 @@ import dev.joaov.security.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,19 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final TokenConfig tokenConfig;
 
-    public AuthController(UserRepository userRepository) {
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, TokenConfig tokenConfig) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.tokenConfig = tokenConfig;
     }
-
+    @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
-        return null;
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
 
+        User user =  (User) authentication.getPrincipal();
+        String token = tokenConfig.generateToken(user);
+        return ResponseEntity.ok(new LoginResponse(token));
     }
-
+    @PostMapping("/register")
     public ResponseEntity<RegisterUserResponse> register(@Valid @RequestBody RegisterUserRequest request) {
         var newUser = new User();
-        newUser.setPassword(request.password());
+        newUser.setPassword(passwordEncoder.encode(request.password()));
         newUser.setEmail(request.email());
         newUser.setName(request.name());
 
